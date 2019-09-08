56d96a34977cb96db0375d7223a9e4056759e998
Josh Sample <joshua.sample1@globalpay.com>
2019-09-08 18:58:07 -0400

    Changed git repo to encompass py and js

---
+++ react-tracks-client/src/pages/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Query } from 'react-apollo';
+import { gql } from 'apollo-boost';
+import withStyles from "@material-ui/core/styles/withStyles";
+
+import SearchTracks from '../components/Track/SearchTracks';
+import TrackList from '../components/Track/TrackList';
+import CreateTrack from '../components/Track/CreateTrack';
+
+const App = ({ classes }) => {
+  return (
+  <div classNames={classes.container}>
+    <SearchTracks />
+    <CreateTrack />
+    <TrackList />
+  </div>
+  )
+};
+
+const styles = theme => ({
+  container: {
+    margin: "0 auto",
+    maxWidth: 960,
+    padding: theme.spacing.unit * 2
+  }
+});
+
+export default withStyles(styles)(App);
